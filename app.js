bd63ed226c7a014cbc9d3e49ccf772295fdd7ce1
OscarMendoza-Code <oscar.mendoza.27@gmail.com>
2025-03-17 14:33:48 -0600

    Concluido app.js

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//Variables
+let inputNombre = document.getElementById("amigo");
+let listaNombres = [];
+let listaBoton = document.getElementById("listaAmigos");
+let mensajeResultado = document.getElementById("resultado");
+
+function agregarAmigo() {
+    //agrega nombre a la lista
+    listaNombres.push(inputNombre.value);
+    console.log(listaNombres);
+    //muestra el alert si el valor esta vacio
+    if (inputNombre.value == ""){
+        alert("Por favor, inserte un nombre")
+    }
+    //muestra el nombre en pantalla
+    listaBoton.innerHTML += `<li>${inputNombre.value}</li>`
+    //limpia el imput
+    document.querySelector('#amigo').value = '';
+}
+
+function sortearAmigo(){
+    let random = Math.floor(Math.random()* listaNombres.length);
+    console.log(random)
+    
+    let amigoSecreto = listaNombres[random];
+  
+        mensajeResultado.innerHTML = `<li>Tu amigo Secreto es ${amigoSecreto}</li>`;
+}
